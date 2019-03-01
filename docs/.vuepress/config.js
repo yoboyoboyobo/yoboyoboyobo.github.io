de5a28a20c40e65a1368fb17fae2d5761aad2665
yoboyoboyobo <isyoboyoboyobo@gmail.com>
2019-03-01 19:49:59 +0800

    test: deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,5 @@
-const basePath = 'dist'
-
 module.exports = {
+	dest: 'public',
   title: "Welcome to Yobo's blog～*･゜ﾟ･*:.｡..｡.:*･'(*ﾟ▽ﾟ*)'･*:.｡. .｡.:*･゜ﾟ･*", 
   description: 'Yobo Blog',
-  base: `/${basePath}/`,
 }

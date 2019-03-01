43c42b7436559a8a74629c3cd785788fde3435c4
yoboyoboyobo <isyoboyoboyobo@gmail.com>
2019-03-01 19:09:05 +0800

    test: config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,7 @@
+const basePath = 'dist'
+
 module.exports = {
   title: "Welcome to Yobo's blog～*･゜ﾟ･*:.｡..｡.:*･'(*ﾟ▽ﾟ*)'･*:.｡. .｡.:*･゜ﾟ･*", 
   description: 'Yobo Blog',
-  base: '/',
+  base: `/${basePath}/`,
 }
